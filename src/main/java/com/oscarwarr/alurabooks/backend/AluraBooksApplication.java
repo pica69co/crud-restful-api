25275028e6ef26ce1d6e0efcada038f50577ee6c
pica69co <oscarwarrieta@gmail.com>
2024-05-16 16:06:39 -0500

    Initial commit

---
+++ src/main/java/com/oscarwarr/alurabooks/backend/AluraBooksApplication.java
@@ -0,0 +1,13 @@
+package com.oscarwarr.alurabooks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarwarr/alurabooks/backend/AluraBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarwarr.alurabooks.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
